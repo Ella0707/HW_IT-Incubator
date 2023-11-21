3c2b0522127dfcc7e142461b208c937d65e5a108
Ella0707 <pakelvi77@gmail.com>
2023-11-21 13:03:51 +0300

    HW-3

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import './App.css';
 import { HW1 } from './HW/1/HW1';
 // import Filter from "./microtasks/Filter";
 import { HW2 } from './HW/2/HW2';
-// import { HW3 } from './HW/3/HW3';
+// import FilterComponent from "./microtasks/Filter/FilterComponent";
+// import Filter from "./microtasks/Filter";
+import { HW3 } from './HW/3/HW3';
 // import { HW4 } from './HW/4/HW4';
 
 function App() {
@@ -10,9 +12,10 @@ function App() {
     <div className="App">
       <HW1 />
       <HW2 />
-      {/*<HW3 />*/}
+      <HW3 />
       {/*<HW4 />*/}
       {/*<Filter/>*/}
+      {/*<FilterComponent/>*/}
     </div>
   );
 }
